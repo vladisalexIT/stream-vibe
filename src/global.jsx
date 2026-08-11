d65d240ebd3d699a65138880b7523c2edc4a6438
vladisalex-it <vl4disalex@gmail.com>
2026-08-11 17:32:27 +0300

    feat: make responsive banner section and update Content

--- src/global.jsx
+++ src/global.jsx
@@ -3,6 +3,7 @@ import Content from "@/layouts/Content"
 import Footer from "@/layouts/Footer"
 import Header from "@/layouts/Header"
 import { Head } from "minista"
+import Banner from './sections/Banner'
 
 export default function (props) {
     const { 
@@ -23,8 +24,9 @@ export default function (props) {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Header url={url} isFixed={isHeaderFixed} />
-      <Content>
+      <Content isResetPaddingTop={isHeaderFixed}>
         {children}
+        <Banner />
       </Content>
       <Footer />
     </>
--- src/layouts/Content/Content.jsx
+++ src/layouts/Content/Content.jsx
@@ -1,9 +1,15 @@
+import classNames from 'classnames'
 import './Content.scss'
 
 const Content = (props) => {
-    const { children } = props
+    const {
+        children,
+        isResetPaddingTop = false,
+    } = props
     return (
-        <main className='content'>
+        <main className={classNames('content', {
+            'content--reset-padding-top': isResetPaddingTop,
+        })}>
             {children}
         </main>
     )
