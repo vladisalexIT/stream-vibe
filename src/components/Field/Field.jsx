8af701333f652de6d6208211bb8752b94f85d01e
vladisalex-it <vl4disalex@gmail.com>
2026-08-15 12:01:10 +0300

    feat(support): complete support form page and add form components

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,60 @@
+import './Field.scss'
+import getIdFromTitle from '@/utils/getIdFromTitle'
+import classNames from 'classnames'
+
+const Field = (props) => {
+  const {
+    className,
+    id = getIdFromTitle(props.label),
+    label,
+    /**
+     * undefined (default) | 'email | 'textarea'
+     */
+    type,
+    placeholder,
+    isRequired,
+    inputMode,
+    mask,
+  } = props
+
+  const Component = type === 'textarea'
+    ? 'textarea'
+    : 'input'
+
+  const extraAttrs = {}
+
+  if (mask) {
+    extraAttrs['data-js-input-mask'] = mask
+  }
+
+  return (
+    <div
+      className={classNames(className, 'field')}
+    >
+      <label
+        className="field__label"
+        htmlFor={id}
+      >
+        {label} {isRequired && (
+          <span
+            className="field__required-star"
+            aria-hidden="true"
+          >*</span>
+        )}
+      </label>
+      <div className="field__body">
+        <Component
+          className="field__control"
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+          inputMode={inputMode}
+          {...extraAttrs}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Field
